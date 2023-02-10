f927012b944106393c956fc46e7e349ec0a07d3e
fu-jw <1151242757@qq.com>
2023-02-10 16:41:54 +0800

    整合日志框架log4j2，并测试

--- spring6-first/src/test/java/com/fredo/spring6/TestUser.java
+++ spring6-first/src/test/java/com/fredo/spring6/TestUser.java
@@ -1,10 +1,14 @@
 package com.fredo.spring6;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestUser {
+
+    private Logger logger = LoggerFactory.getLogger(TestUser.class);
 
     // 测试创建对象
     @Test
@@ -17,5 +21,29 @@ public class TestUser {
         User user = (User) ioc.getBean("user");
         // 3.使用对象调用方法
         user.add();
+
+        logger.info("执行成功");
     }
+    /**
+     * 入门案例小结：
+     * Spring 出现以前，创建对象需要无参构造器：
+     *  类名 对象 = new 类名()；
+     * Spring 实现了 IOC（控制反转），可以根据配置文件，在程序启动时就创建好对象，并放在IOC容器中供用户使用
+     * 将创建对象的权利反转给Spring，省去用户创建对象的操作
+     *
+     * 1.Spring 创建对象的步骤：
+     *  1 加载配置文件 beans.xml
+     *  2 对配置文件进行解析
+     *  3 获取xml文件中 bean 标签的属性值（常见有id，clas。。。）
+     *  4 使用反射，根据全类名(class的值)创建对象
+     *
+     * 2.为什么不自己new对象
+     *  1 后期可能居多的类，自己难以管理
+     *  2 交给Spring 方便管理
+     *
+     * 3.Spring 创建的对象放在哪
+     *  在 beanDefinitionMap中，是一个Map<String, BeanDefinition>
+     *  key     是 bean 的 id
+     *  value   是 bean 的定义(或描述信息) BeanDefinition
+     */
 }
