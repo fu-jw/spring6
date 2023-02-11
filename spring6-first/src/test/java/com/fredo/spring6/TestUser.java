3155871ed606f8bba8f12e90463d8f9ffcc9970c
fu-jw <1151242757@qq.com>
2023-02-11 14:51:04 +0800

    IoC概述

--- spring6-first/src/test/java/com/fredo/spring6/TestUser.java
+++ spring6-first/src/test/java/com/fredo/spring6/TestUser.java
@@ -3,8 +3,11 @@ package com.fredo.spring6;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.swing.*;
 
 public class TestUser {
 
@@ -50,5 +53,36 @@ public class TestUser {
      *  value   是 bean 的定义(或描述信息) BeanDefinition
      */
 
+    /**
+     * IoC
+     *  IoC 是 Inversion of Control 的简写，“控制反转”，
+     *  是一种思想，不是一门技术
+     *  是重要的面向对象的编程法则，可以指导我们设计出松耦合的程序
+     *
+     *  IoC 思想的落地实现是 IoC 容器
+     *
+     *  Spring 通过 IoC 容器来管理所有 Java对象的实例化和初始化，控制对象之间的依赖关系
+     *  将由 IoC 容器管理的对象称为 Bean，与new出来的普通对象无任何本质区别
+     *
+     * IoC容器在Spring的实现
+     *  Spring 的 IoC 容器就是 IoC思想的一个落地的产品实现。
+     *  IoC容器中管理的组件也叫做 bean。在创建 bean 之前，首先需要创建IoC 容器。
+     *  Spring 提供了IoC 容器的两种实现方式：
+     *  1.BeanFactory
+     *  2.ApplicationContext
+     *
+     * IoC容器加载 Bean 对象的流程
+     *  0.配置Bean定义信息，可以在XML配置文件中，或者注解
+     *  1.通过接口 BeanDefinitionReader 读取配置信息
+     *  2.在容器中，将Bean信息实例化，工厂(BeanFactory) + 反射
+     *  3.对象初始化成最终的Bean对象，将实例化的空对象中的属性信息进行初始化
+     *  4.getBean(“name”) 即可获取Bean对象
+     *
+     * DI
+     *  Dependency Injection 依赖注入，实现控制反转的具体实现
+     *  DI 的方式有两种：
+     *  1.setter 注入
+     *  2.构造器注入
+     */
 
 }
