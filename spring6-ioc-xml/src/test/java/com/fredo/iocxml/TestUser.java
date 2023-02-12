dbb766d445146e023a1b812231df6892e5c09397
fu-jw <1151242757@qq.com>
2023-02-12 18:09:32 +0800

    FactoryBean 的作用

--- spring6-ioc-xml/src/test/java/com/fredo/iocxml/TestUser.java
+++ spring6-ioc-xml/src/test/java/com/fredo/iocxml/TestUser.java
@@ -137,4 +137,29 @@ public class TestUser {
         System.out.println("生命周期：6、通过IOC容器获取bean并使用");
         ioc.close();
     }
+
+    // 6.接口 FactoryBean 常用于整合其他框架
+    /**
+     * Spring 中有两种类型的Bean，一种是普通Bean，另一种是工厂Bean 即 FactoryBean。
+     * FactoryBean跟普通Bean不同，其返回的对象不是指定类的一个实例，而是该FactoryBean的getObject方法所返回的对象。
+     * 创建出来的对象是否属于单例由isSingleton中的返回决定。
+     *
+     * 一般情况下，Spring通过反射机制利用<bean>的class属性指定实现类实例化Bean，
+     * 在某些情况下，实例化Bean过程比较复杂，如果按照传统的方式，则需要在<bean>中提供大量的配置信息。
+     * 配置方式的灵活性是受限的，这时采用编码的方式可能会得到一个简单的方案。
+     * Spring为此提供了一个 FactoryBean 的工厂类接口，用户可以通过实现该接口定制实例化Bean的逻辑。
+     * FactoryBean 接口对于Spring框架来说占用重要的地位，Spring自身就提供了70多个FactoryBean的实现。
+     * 它们隐藏了实例化一些复杂Bean的细节，给上层应用带来了便利。
+     * 从Spring3.0开始，FactoryBean开始支持泛型，即接口声明改为FactoryBean<T>的形式
+     *
+     * 以Bean结尾，表示它是一个Bean，不同于普通Bean的是：它是实现了FactoryBean<T>接口的Bean，
+     * 根据该Bean的ID从BeanFactory中获取的实际上是FactoryBean的getObject()返回的对象，
+     * 而不是FactoryBean本身，如果要获取FactoryBean对象，请在id前面加一个&符号来获取。
+     *
+     * FactoryBean 通常是用来创建比较复杂的bean，一般的bean 直接用xml配置即可，
+     * 但如果一个bean的创建过程中涉及到很多其他的bean 和复杂的逻辑，用xml配置比较困难，这时可以考虑用FactoryBean。
+     *
+     * 很多开源项目在集成Spring 时都使用到FactoryBean，
+     * 比如 MyBatis3 提供 mybatis-spring项目中的 org.mybatis.spring.SqlSessionFactoryBean：
+     */
 }
