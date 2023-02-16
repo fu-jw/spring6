6adabd898b54a9e5b263fb3c0c2709a644231462
fu-jw <1151242757@qq.com>
2023-02-16 09:46:09 +0800

    aop--引入计算机案例

---
+++ spring6-aop/src/main/java/com/fredo/calc/Calculator.java
@@ -0,0 +1,13 @@
+package com.fredo.calc;
+
+public interface Calculator {
+    
+    int add(int i, int j);
+    
+    int sub(int i, int j);
+    
+    int mul(int i, int j);
+    
+    int div(int i, int j);
+    
+}
